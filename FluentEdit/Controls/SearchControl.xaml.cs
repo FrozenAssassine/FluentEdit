86a3341afe2c9be2c326ca0848e44bf94cfbee96
Julius Kirsch <Julius@skirsch.de>
2025-05-25 16:34:07 +0200

    fix: search

--- FluentEdit/Controls/SearchControl.xaml.cs
+++ FluentEdit/Controls/SearchControl.xaml.cs
@@ -7,224 +7,218 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using TextControlBoxNS;
 using Microsoft.UI;
-using TextControlBox_TestApp.Helper;
 
-namespace FluentEdit.Controls
+namespace FluentEdit.Controls;
+
+public sealed partial class SearchControl : UserControl
 {
-    public sealed partial class SearchControl : UserControl
+    private TextControlBox currentTextbox = null;
+    public bool searchOpen = false;
+
+    private SearchWindowState searchWindowState = SearchWindowState.Hidden;
+
+    public SearchControl()
     {
-        private TextControlBox currentTextbox = null;
-        private SearchWindowState searchWindowState = SearchWindowState.Hidden;
+        this.InitializeComponent();
+    }
 
-        public SearchControl()
-        {
-            this.InitializeComponent();
-        }
+    private void BeginSearch(string searchword, bool matchCase, bool wholeWord)
+    {
+        var res = currentTextbox.BeginSearch(searchword, wholeWord, matchCase);
+        ColorWindowBorder(res);
+    }
+    private void ToggleVisibility(bool visible)
+    {
+        textToReplaceTextBox.Visibility = ReplaceAllButton.Visibility =
+            StartReplaceButton.Visibility = ConvertHelper.BoolToVisibility(visible);
+    }
+    private void ColorWindowBorder(SearchResult result)
+    {
+        SearchWindow.BorderBrush = new SolidColorBrush(result == SearchResult.Found ? Colors.Green : Colors.Red);
+    }
+    private void HideWindow()
+    {
+        hideSearchAnimation.Begin();
+        searchWindowState = SearchWindowState.Hidden;
+    }
+    private void ShowWindow()
+    {
+        this.Visibility = Visibility.Visible;
+        showSearchAnimation.Begin();
+        searchWindowState = SearchWindowState.Default;
+    }
+    private void ExpandReplace()
+    {
+        expandSearchAnimation.Begin();
+        searchWindowState = SearchWindowState.Expanded;
+    }
+    private void CollapseReplace()
+    {
+        collapseSearchAnimation.Begin();
+        searchWindowState = SearchWindowState.Default;
+    }
 
-        private void BeginSearch(string searchword, bool matchCase, bool wholeWord)
-        {
-            if (currentTextbox == null)
-                return;
+    public void ShowSearch(TextControlBox textbox)
+    {
+        //if (currentTextbox != null && currentTextbox != textbox)
+        //{
+        //    currentTextbox.EndSearch();
+        //    this.searchWindowState = SearchWindowState.Hidden;
+        //}
 
-            try
-            {
-                //begin the search
-                var res = currentTextbox.BeginSearch(searchword, wholeWord, matchCase);
-                ColorWindowBorder(res);
-            }
-            catch
-            {
-                //Due to bug in TextControlBox
-            }
-        }
-        private void ToggleVisibility(bool visible)
-        {
-            textToReplaceTextBox.Visibility = ReplaceAllButton.Visibility =
-                StartReplaceButton.Visibility = ConvertHelper.BoolToVisibility(visible);
-        }
-        private void ColorWindowBorder(SearchResult result)
-        {
-            SearchWindow.BorderBrush = new SolidColorBrush(result == SearchResult.Found ? Colors.Green : Colors.Red);
-        }
-        private void hideWindow()
-        {
-            hideSearchAnimation.Begin();
-            searchWindowState = SearchWindowState.Hidden;
-        }
-        private void showWindow()
-        {
-            this.Visibility = Visibility.Visible;
-            showSearchAnimation.Begin();
-            searchWindowState = SearchWindowState.Default;
-        }
-        private void expandReplace()
-        {
-            expandSearchAnimation.Begin();
-            searchWindowState = SearchWindowState.Expanded;
-        }
-        private void collapseReplace()
-        {
-            collapseSearchAnimation.Begin();
-            searchWindowState = SearchWindowState.Default;
-        }
+        currentTextbox = textbox;
+        searchOpen = true;
 
-        public void ShowSearch(TextControlBox textbox)
+        if (searchWindowState == SearchWindowState.Expanded)
         {
-            if (textbox == null)
-                return;
-            currentTextbox = textbox;
-
-            //hide the window when in search state or show it when in hidden state or show it when hidden:
-            if (searchWindowState == SearchWindowState.Default)
-                hideWindow();
-            else if (searchWindowState == SearchWindowState.Expanded)
-                collapseReplace();
-            else
-            {
-                showWindow();
-                collapseReplace();
-            }
-
-            if (textbox.HasSelection && textbox.CalculateSelectionPosition().Length < 200)
-                textToFindTextbox.Text = textbox.SelectedText;
-
-            textToFindTextbox.Focus(FocusState.Keyboard);
-            textToFindTextbox.SelectAll();
+            CollapseReplace();
         }
-        public void ShowReplace(TextControlBox textbox)
+        else if (searchWindowState == SearchWindowState.Hidden)
         {
-            if (textbox == null)
-                return;
-            currentTextbox = textbox;
-
-            //hide the window when in replace state or expand it when in search state or show it when hidden:
-            if (searchWindowState == SearchWindowState.Expanded)
-                hideWindow();
-            else if (searchWindowState == SearchWindowState.Default)
-                expandReplace();
-            else
-            {
-                showWindow();
-                expandReplace();
-            }
-
-            if (textbox.HasSelection && textbox.CalculateSelectionPosition().Length < 200)
-                textToFindTextbox.Text = textbox.SelectedText;
-
-            textToReplaceTextBox.Focus(FocusState.Keyboard);
-            textToReplaceTextBox.SelectAll();
-            textToFindTextbox.Focus(FocusState.Keyboard);
-            textToFindTextbox.SelectAll();
-        }
-        public void Close()
-        {
-            currentTextbox.EndSearch();
-            hideWindow();
-            currentTextbox = null;
+            ShowWindow();
+            CollapseReplace();
         }
 
-        private void ReplaceTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        if (currentTextbox.HasSelection && currentTextbox.CalculateSelectionPosition().Length < 200)
         {
-            if (e.Key == VirtualKey.Enter)
-            {
-                ReplaceCurrentButton_Click(null, null);
-            }
-        }
-        private void SearchTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
-        {
-            if (currentTextbox == null)
-                return;
-
-            //Search down on Enter and up on Shift + Enter//
-            var shift = KeyHelper.IsKeyPressed(VirtualKey.Shift);
-            if (e.Key == VirtualKey.Enter)
-            {
-                if (shift)
-                    currentTextbox.FindPrevious();
-                else
-                    currentTextbox.FindNext();
-            }
-        }
-        private void SearchUpButton_Click(object sender, RoutedEventArgs e)
-        {
-            currentTextbox?.FindPrevious();
-        }
-        private void SearchDownButton_Click(object sender, RoutedEventArgs e)
-        {
-            currentTextbox?.FindNext();
-        }
-        private void FindMatchCaseButton_Click(object sender, RoutedEventArgs e)
-        {
-            textToFindTextbox_TextChanged(null, null); //update the live search
-        }
-        private void FindWholeWordButton_Click(object sender, RoutedEventArgs e)
-        {
-            textToFindTextbox_TextChanged(null, null); //update the live search
-        }
-        private void ReplaceAllButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (currentTextbox == null)
-                return;
-
-            var res = currentTextbox.ReplaceAll(
-                textToFindTextbox.Text,
-                textToReplaceTextBox.Text,
-                FindMatchCaseButton.IsChecked ?? false,
-                FindWholeWordButton.IsChecked ?? false
-                );
-
-            ColorWindowBorder(res);
-        }
-        private void ReplaceCurrentButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (currentTextbox == null)
-                return;
-
-            currentTextbox.ReplaceNext(textToReplaceTextBox.Text);
-        }
-        private void SearchWindow_CloseButtonClick(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-        private void ExpandSearchBoxForReplaceButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (searchWindowState == SearchWindowState.Expanded)
-                ShowSearch(currentTextbox);
-            else
-                ShowReplace(currentTextbox);
-        }
-        private void TextBoxes_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (sender is TextBox tb)
-            {
-                tb.SelectAll();
-            }
+            textToFindTextbox.Text = currentTextbox.SelectedText;
         }
 
-        private void textToFindTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        textToFindTextbox.Focus(FocusState.Keyboard);
+        textToFindTextbox.SelectAll();
+    }
+
+    public void ShowReplace(TextControlBox textbox)
+    {
+        currentTextbox = textbox;
+
+        if (searchWindowState == SearchWindowState.Default)
         {
-            BeginSearch(textToFindTextbox.Text, FindMatchCaseButton.IsChecked ?? false, FindWholeWordButton.IsChecked ?? false);
+            ExpandReplace();
+        }
+        else if (searchWindowState == SearchWindowState.Hidden)
+        {
+            ShowWindow();
+            ExpandReplace();
         }
 
-        private void collapseSearchAnimation_Completed(object sender, object e)
+
+        if (currentTextbox.HasSelection && currentTextbox.CalculateSelectionPosition().Length < 200)
         {
-            ToggleVisibility(false);
+            textToFindTextbox.Text = currentTextbox.SelectedText;
         }
 
-        private void expandSearchAnimation_Completed(object sender, object e)
-        {
-            ToggleVisibility(true);
-        }
+        textToReplaceTextBox.Focus(FocusState.Keyboard);
+        textToReplaceTextBox.SelectAll();
+        textToFindTextbox.Focus(FocusState.Keyboard);
+        textToFindTextbox.SelectAll();
+    }
 
-        private void hideSearchAnimation_Completed(object sender, object e)
+    public void Close()
+    {
+        if (!searchOpen || currentTextbox == null)
+            return;
+
+        searchOpen = false;
+        currentTextbox.EndSearch();
+        HideWindow();
+        currentTextbox.Focus(FocusState.Programmatic);
+
+        currentTextbox = null;
+    }
+
+    private void UpdateSearch()
+    {
+        BeginSearch(textToFindTextbox.Text, FindMatchCaseButton.IsChecked ?? false, FindWholeWordButton.IsChecked ?? false);
+    }
+
+    private void ReplaceTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key == VirtualKey.Enter)
         {
-            this.Visibility = Visibility.Collapsed;
+            ReplaceCurrentButton_Click(null, null);
         }
     }
-    public enum SearchWindowState
+    private void SearchTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
     {
-        Expanded, //replace and search
-        Default, //only search
-        Hidden //not visible
+        if (currentTextbox == null)
+            return;
+
+        //Search down on Enter and up on Shift + Enter//
+        var shift = KeyHelper.IsKeyPressed(VirtualKey.Shift);
+        if (e.Key == VirtualKey.Enter)
+        {
+            if (shift)
+                currentTextbox.FindPrevious();
+            else
+                currentTextbox.FindNext();
+        }
     }
+    private void SearchUpButton_Click(object sender, RoutedEventArgs e)
+    {
+        currentTextbox.FindPrevious();
+    }
+    private void SearchDownButton_Click(object sender, RoutedEventArgs e)
+    {
+        currentTextbox.FindNext();
+    }
+    private void ReplaceAllButton_Click(object sender, RoutedEventArgs e)
+    {
+        var res = currentTextbox.ReplaceAll(
+            textToFindTextbox.Text,
+            textToReplaceTextBox.Text,
+            FindMatchCaseButton.IsChecked ?? false,
+            FindWholeWordButton.IsChecked ?? false
+            );
+
+        ColorWindowBorder(res);
+    }
+    private void ReplaceCurrentButton_Click(object sender, RoutedEventArgs e)
+    {
+        var res = currentTextbox.ReplaceNext(textToReplaceTextBox.Text);
+        ColorWindowBorder(res);
+    }
+    private void SearchWindow_CloseButtonClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+    private void ExpandSearchBoxForReplaceButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (searchWindowState == SearchWindowState.Expanded)
+            ShowSearch(currentTextbox);
+        else
+            ShowReplace(currentTextbox);
+    }
+    private void TextBoxes_GotFocus(object sender, RoutedEventArgs e)
+    {
+        (sender as TextBox)?.SelectAll();
+    }
+
+    private void TextToFindTextbox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        UpdateSearch();
+    }
+    private void SearchProperties_Changed(object sender, RoutedEventArgs e)
+    {
+        UpdateSearch();
+    }
+
+    private void CollapseSearchAnimation_Completed(object sender, object e)
+    {
+        ToggleVisibility(false);
+    }
+    private void ExpandSearchAnimation_Completed(object sender, object e)
+    {
+        ToggleVisibility(true);
+    }
+    private void HideSearchAnimation_Completed(object sender, object e)
+    {
+        this.Visibility = Visibility.Collapsed;
+    }
+}
+public enum SearchWindowState
+{
+    Expanded, //replace and search
+    Default, //only search
+    Hidden //not visible
 }
